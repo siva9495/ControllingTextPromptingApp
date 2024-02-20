1959cd6560abac64398ed00bedf7e1b09dba88d5
Kadiyala Siva Prasad <sivaprasad.ksp01@gmail.com>
2024-02-20 11:22:05 +0530

    Adding the QR Code Scanner

--- app/src/main/java/com/example/senderapptextprompting/MainActivity.java
+++ app/src/main/java/com/example/senderapptextprompting/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.senderapptextprompting;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnQRScanner = findViewById(R.id.scanQRCodeButton);
+
+        btnQRScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanQRCodeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void startScrolling(View view) {
